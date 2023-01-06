e7c5b98e76395d98c8dd5ddf1549a8f7da552f0d
tlswlgns14 <tlswlgns14@minigate.net>
2023-01-06 17:40:52 +0900

    dd

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,10 +1,15 @@
-import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import { useState } from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
-
+import data from './data.js';
 
 function App() {
+
+  let [shoes] = useState(data)
+
   return (
     <div className="App">
+      
       <Navbar bg="light" variant="light">
         <Container>
         <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
@@ -20,12 +25,20 @@ function App() {
       <div className='container'>
         <div className='row'>
           <div className='col-md-4'>
-            <img src='https://codingapple1.github.io/shop/shoes1/jpg'/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <img src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c62dc34f-c216-4029-b476-232651c03868/%EB%8D%A9%ED%81%AC-%EB%A1%9C%EC%9A%B0-se-%EB%82%A8%EC%84%B1-%EC%8B%A0%EB%B0%9C-BPKVQd11.png" width="80%"/>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
           </div>
-          <div className='col-md-4'>안녕</div>
-          <div className='col-md-4'>안녕</div>
+          <div className='col-md-4'>
+            <img src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c62dc34f-c216-4029-b476-232651c03868/%EB%8D%A9%ED%81%AC-%EB%A1%9C%EC%9A%B0-se-%EB%82%A8%EC%84%B1-%EC%8B%A0%EB%B0%9C-BPKVQd11.png" width="80%"/>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
+          </div>
+          <div className='col-md-4'>
+            <img src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c62dc34f-c216-4029-b476-232651c03868/%EB%8D%A9%ED%81%AC-%EB%A1%9C%EC%9A%B0-se-%EB%82%A8%EC%84%B1-%EC%8B%A0%EB%B0%9C-BPKVQd11.png" width="80%"/>
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
+          </div>
         </div>
       </div>
       
